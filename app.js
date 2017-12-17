64ab973f4e6f514ef4255f9a5699fb332de05ff5
Toby Weed <tobyweed@gmail.com>
2017-12-17 03:46:40 +0000

    add borders to thumbnails

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser")
+    
+    
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+
+app.get("/",function(req,res){
+    res.render("landing");
+})
+
+app.get("/about",function(req,res){
+    res.render("about");
+})
+
+app.get("/projects",function(req,res){
+    res.render("projects");
+})
+
+app.get("/essays",function(req,res){
+    res.render("essays");
+})
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("server is listening");
+})
