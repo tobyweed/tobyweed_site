f353ac0d942eba921acbc35936a1bd92de0e2ecb
Toby Weed <tobyweed@gmail.com>
2018-10-15 16:41:12 -0400

    update

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-var express = require("express"),
-    app = express(),
-    bodyParser = require("body-parser")
-    
-    
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + "/public"));
-app.set("view engine", "ejs");
+var express = require('express'),
+	app = express(),
+	bodyParser = require('body-parser');
 
-app.get("/",function(req,res){
-    res.render("landing");
-})
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
 
-app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("server is listening");
-})
+app.get('/', function(req, res) {
+	res.render('landing');
+});
+
+// app.listen(process.env.PORT, process.env.IP, function(){
+//     console.log("server is listening");
+// })
+app.listen(3000, process.env.IP, function() {
+	console.log('server is listening');
+});
